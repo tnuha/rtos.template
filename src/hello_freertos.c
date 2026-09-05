15fe27a94fe038f7b510e291e09a13c67482ddcb
Ashton Hunt <tnuha@proton.me>
2026-09-05 15:24:13 -0600

    fix(blinker-rate): decrease the delay between individual LED blinks
    Blinker rate is at a currently undesirable 500 ms delay between blinks.
    Raising the blink rate to 100 ms between blinks gives some insights
    into FreeRTOS's scheduling.
    This is fun and fucked up.

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -27,7 +27,7 @@ void blink_task(__unused void *params) {
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
     if (count++ % 11)
       on = !on;
-    vTaskDelay(500);
+    vTaskDelay(100);
   }
 }
 
